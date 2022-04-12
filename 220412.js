d806438ffa8ee92055c531c5e1aac247eabef63f
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-04-12 16:39:36 +0900

    Update 220412.js

--- 220412.js
+++ 220412.js
@@ -29,9 +29,9 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    for (i=0; i<b;i++) {
+    for (i=0; i<b; i++) {
         let star = ""                          // 변수 선언.
-        for (j=0; j<a;j++) {
+        for (j=0; j<a; j++) {
             star += '*'
         }
         console.log(star)
