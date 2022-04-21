01cc0a24b9fd4e35e3bf9321047e678457c3eea8
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-04-21 22:19:09 +0900

    Update 220416-02.js

--- 220416-02.js
+++ 220416-02.js
@@ -1,1 +1,46 @@
+/*
+[플랫폼]
+프로그래머스
 
+[난이도]
+레벨 1
+
+[문제]
+코딩테스트 연습 > 제일 작은 수 제거하기
+
+[링크]
+https://programmers.co.kr/learn/courses/30/lessons/12935
+*/
+
+
+// 기본 틀
+function solution(arr) {
+    var answer = [];
+    return answer;
+}
+
+
+
+// 풀이
+function solution(arr) {
+    if(arr.length === 1) {
+        return [-1]
+    } else {
+        arr.splice(arr.indexOf(Math.min(...arr)),1)
+    }
+    return  arr
+}
+
+
+
+// 다른 풀이
+function solution(arr) {
+    arr.splice(arr.indexOf(Math.min(...arr)),1);
+    if(arr.length<1)return[-1];
+    return arr;
+}
+
+
+/*
+
+*/
