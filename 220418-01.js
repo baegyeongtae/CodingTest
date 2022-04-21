cc5a9bc22b14c762b0ce0c7757986d92833fc7df
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-04-21 22:23:14 +0900

    Update 220418-01.js

--- 220418-01.js
+++ 220418-01.js
@@ -1,1 +1,70 @@
 
+/*
+[플랫폼]
+프로그래머스
+
+[난이도]
+레벨 1
+
+[문제]
+코딩테스트 연습 > 정수 제곱근 판별
+
+[링크]
+https://programmers.co.kr/learn/courses/30/lessons/12934
+*/
+
+
+// 기본 틀
+function solution(n) {
+    var answer = 0;
+    return answer;
+}
+
+
+
+// 풀이
+function solution(n) {
+    var answer = Math.sqrt(n);
+    return answer%1 === 0 ? (answer+1)**2 : -1;
+}
+
+
+
+// 다른 풀이 1
+function nextSqaure(n){
+  var result = 0;
+  var x = 0;
+  while (x*x < n){
+    x++;
+  }
+  if (x*x == n){
+    x++;
+    result = x*x; 
+  }else{
+    result = 'no';
+  }
+
+  return result;
+}
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log("결과 : " + nextSqaure(121));
+
+
+
+// 다른 풀이 2
+function nextSqaure(n){
+    var result = 0;
+    var n = Math.sqrt(n);
+    result = Number.isInteger(n) ? Math.pow(n+1, 2) : 'no';
+    return result;
+}
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log("결과 : " + nextSqaure(121));
+console.log("결과 : " + nextSqaure(3));
+
+
+/*
+
+*/
