43a808152ce1628e7ddc81c5f777156090d70445
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-04-21 22:25:54 +0900

    Update 220418-02.js

--- 220418-02.js
+++ 220418-02.js
@@ -1,1 +1,61 @@
 
+/*
+[플랫폼]
+프로그래머스
+
+[난이도]
+레벨 1
+
+[문제]
+코딩테스트 연습 > 정수 내림차순으로 배치하기
+
+[링크]
+https://programmers.co.kr/learn/courses/30/lessons/12933
+*/
+
+
+// 기본 틀
+function solution(n) {
+    var answer = 0;
+    return answer;
+}
+
+
+
+// 풀이
+function solution(n) {
+    var answer = [];
+    let num_list = [];
+    let list = [];
+    num_list = String(n).split("")
+    for(i=0; i<num_list.length; i++) {
+        list.push(Number(num_list[i]))
+    }
+    list.sort(function(a, b) {
+        return b - a;
+    })
+    for(i=0; i<list.length; i++) {
+        answer.push(String(list[i]))
+    }
+    answer = Number(answer.join(''))
+    return answer
+}
+
+
+
+// 다른 풀이 
+function solution(n) {
+  const newN = n + "";
+  const newArr = newN
+    .split("")
+    .sort()
+    .reverse()
+    .join("");
+
+  return +newArr;
+}
+
+
+/*
+
+*/
