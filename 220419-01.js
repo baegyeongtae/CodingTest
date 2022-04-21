ec10bd4bd22f04e5ff9e4debcba669712e1cd04d
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-04-21 22:35:03 +0900

    Update 220419-01.js

--- 220419-01.js
+++ 220419-01.js
@@ -1,1 +1,55 @@
 
+/*
+[플랫폼]
+프로그래머스
+
+[난이도]
+레벨 1
+
+[문제]
+코딩테스트 연습 > 자연수 뒤집어 배열로 만들기
+
+[링크]
+https://programmers.co.kr/learn/courses/30/lessons/12932
+*/
+
+
+// 기본 틀
+function solution(n) {
+    var answer = [];
+    return answer;
+}
+
+
+
+// 풀이
+function solution(n) {
+    let arr = n.toString().split('').reverse();
+    for(i=0; i<arr.length; i++) {
+        arr[i] = Number(arr[i])
+    }
+    return arr;
+}
+
+
+
+// 다른 풀이 
+function solution(n) {
+    // 문자풀이
+    // return (n+"").split("").reverse().map(v => parseInt(v));
+
+    // 숫자풀이
+    var arr = [];
+
+    do {
+        arr.push(n%10);
+        n = Math.floor(n/10);
+    } while (n>0);
+
+    return arr;
+}
+
+
+/*
+
+*/
