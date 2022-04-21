04d7412fcb5e0926014b28c084a1d6ae55b464cc
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-04-21 23:01:32 +0900

    Update 220420-02.js

--- 220420-02.js
+++ 220420-02.js
@@ -1,1 +1,45 @@
 
+/*
+[플랫폼]
+프로그래머스
+
+[난이도]
+레벨 1
+
+[문제]
+코딩테스트 연습 > 약수의 합
+
+[링크]
+https://programmers.co.kr/learn/courses/30/lessons/12928
+*/
+
+
+// 기본 틀
+function solution(n) {
+    var answer = 0;
+    return answer;
+}
+
+
+
+// 풀이
+function solution(n) {
+    var answer = 0;
+    for(i=1; i<(n+1); i++) {
+        if(n%i === 0) {
+            answer += i
+        }
+    }
+    return answer;
+}
+
+
+
+// 다른 풀이
+function solution(n, a=0, b=0) {
+    return n<=a/2?b:solution(n,a+1,b+=n%a?0:a);
+}
+
+/*
+
+*/
