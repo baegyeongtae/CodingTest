d614a073a37d05b31e15f652cf605e0f18989b65
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-04-23 05:23:56 +0900

    Update 220422-02.js

--- 220422-02.js
+++ 220422-02.js
@@ -1,1 +1,56 @@
 
+/*
+[플랫폼]
+프로그래머스
+
+[난이도]
+레벨 1
+
+[문제]
+코딩테스트 연습 > 문자열을 정수로 바꾸기
+
+[링크]
+https://programmers.co.kr/learn/courses/30/lessons/12925
+*/
+
+
+// 기본 틀
+function solution(n) {
+    var answer = 0;
+    return answer;
+}
+
+
+
+// 풀이
+function solution(s) {
+    return Number(s);
+}
+
+
+
+
+// 다른 풀이 1
+function strToInt(str){
+  return str/1
+  }
+
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log(strToInt("-1234"));
+
+
+
+
+// 다른 풀이 2
+function strToInt(str){
+  return  +str;
+}
+
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log(strToInt("-1234"));
+
+/*
+
+*/
