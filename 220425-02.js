e644b681b0f2fc6aaa5fcd0b6994eaba89719ba2
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-04-28 16:00:05 +0900

    Update 220425-02.js

--- 220425-02.js
+++ 220425-02.js
@@ -1,1 +1,43 @@
 
+
+
+/*
+[플랫폼]
+프로그래머스
+
+[난이도]
+레벨 1
+
+[문제]
+코딩테스트 연습 > 수박수박수박수박수박수?
+
+[링크]
+https://programmers.co.kr/learn/courses/30/lessons/12922
+*/
+
+
+// 기본 틀
+function solution(n) {
+    var answer = 0;
+    return answer;
+}
+
+
+
+// 풀이
+function solution(s) {
+    return Number(s);
+}
+
+
+
+
+// 다른 풀이 
+const waterMelon = n => "수박".repeat(n).slice(0,n);
+
+console.log("n이 3인 경우: "+ waterMelon(3))
+console.log("n이 4인 경우: "+ waterMelon(4))
+
+/*
+
+*/
