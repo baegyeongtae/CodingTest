3a282cb6030996b23a3c32beeda9967817c63233
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-05-04 12:56:46 +0900

    Update 220426-02.js

--- 220426-02.js
+++ 220426-02.js
@@ -9,34 +9,55 @@
 레벨 1
 
 [문제]
-코딩테스트 연습 > 수박수박수박수박수박수?
+코딩테스트 연습 > 연습문제 > 나누어 떨어지는 숫자 배열
 
 [링크]
-https://programmers.co.kr/learn/courses/30/lessons/12922
+https://programmers.co.kr/learn/courses/30/lessons/12910
 */
 
 
 // 기본 틀
-function solution(n) {
-    var answer = 0;
+function solution(arr, divisor) {
+    var answer = [];
     return answer;
 }
 
 
 
 // 풀이
-function solution(s) {
-    return Number(s);
+function solution(arr, divisor) {
+    var answer = [];
+    arr.map((v) => {
+        if(v%divisor === 0) {
+            answer.push(v)
+        }
+    })
+    answer.sort((a,b) => {return a-b})
+    if(answer.length === 0) {return [-1]}
+    return answer;
 }
 
 
 
 
-// 다른 풀이 
-const waterMelon = n => "수박".repeat(n).slice(0,n);
+// 다른 풀이 1
+function solution(arr, divisor) {
+    var answer = arr.filter(v => v%divisor == 0);
+    return answer.length == 0 ? [-1] : answer.sort((a,b) => a-b);
+}
 
-console.log("n이 3인 경우: "+ waterMelon(3))
-console.log("n이 4인 경우: "+ waterMelon(4))
+
+
+// 다른 풀이 2
+function solution(arr, divisor) {
+    var answer = [];
+    arr.map((o) => {
+        o % divisor === 0 && answer.push(o);
+    })
+    return answer.length ? answer.sort((a, b) => a - b) : [-1];
+
+}
+
 
 /*
 
