6140a1ad009833e1bb429f8fbd50d4993be0587b
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-05-04 13:00:33 +0900

    Update 220427-02.js

--- 220427-02.js
+++ 220427-02.js
@@ -9,16 +9,16 @@
 레벨 1
 
 [문제]
-코딩테스트 연습 > 수박수박수박수박수박수?
+코딩테스트 연습 > 연습문제 > 가운데 글자 가져오기
 
 [링크]
-https://programmers.co.kr/learn/courses/30/lessons/12922
+https://programmers.co.kr/learn/courses/30/lessons/12903
 */
 
 
 // 기본 틀
-function solution(n) {
-    var answer = 0;
+function solution(s) {
+    var answer = '';
     return answer;
 }
 
@@ -26,17 +26,32 @@ function solution(n) {
 
 // 풀이
 function solution(s) {
-    return Number(s);
+    var answer = '';
+    if(s.length%2 === 0) {
+        answer += s[(s.length)/2 - 1]
+        answer += s[(s.length)/2]
+    } else {
+        answer += s[(s.length-1)/2]
+    }
+    return answer;
 }
 
 
 
 
-// 다른 풀이 
-const waterMelon = n => "수박".repeat(n).slice(0,n);
+// 다른 풀이 1
+function solution(s) {
+    return s.substr(Math.ceil(s.length / 2) - 1, s.length % 2 === 0 ? 2 : 1);
+}
 
-console.log("n이 3인 경우: "+ waterMelon(3))
-console.log("n이 4인 경우: "+ waterMelon(4))
+
+
+// 다른 풀이 2
+function solution(s) {
+    const mid = Math.floor(s.length/2);
+    return s.length %2 === 1 ? s[mid] : s[mid-1]+s[mid];
+}
+
 
 /*
 
