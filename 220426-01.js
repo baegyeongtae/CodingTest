10eaa280fc6a7060240474e6c28501cd41ef1736
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-05-04 12:54:53 +0900

    Update 220426-01.js

--- 220426-01.js
+++ 220426-01.js
@@ -9,15 +9,15 @@
 레벨 1
 
 [문제]
-코딩테스트 연습 > 수박수박수박수박수박수?
+코딩테스트 연습 > 연습문제 > 두 정수 사이의 합
 
 [링크]
-https://programmers.co.kr/learn/courses/30/lessons/12922
+https://programmers.co.kr/learn/courses/30/lessons/12912
 */
 
 
 // 기본 틀
-function solution(n) {
+function solution(a, b) {
     var answer = 0;
     return answer;
 }
@@ -25,18 +25,45 @@ function solution(n) {
 
 
 // 풀이
-function solution(s) {
-    return Number(s);
+function solution(a, b) {
+    let answer = 0;
+    let list = [];
+    list.push(a, b)
+    list.sort((a, b) => {return a-b})
+    for(let i=list[0]; i<=list[1]; i++) {
+        answer += i
+    }
+    return answer;
 }
 
 
 
 
-// 다른 풀이 
-const waterMelon = n => "수박".repeat(n).slice(0,n);
+// 다른 풀이 1
+function adder(a, b){
+    var result = 0
+    //함수를 완성하세요
 
-console.log("n이 3인 경우: "+ waterMelon(3))
-console.log("n이 4인 경우: "+ waterMelon(4))
+
+    return (a+b)*(Math.abs(b-a)+1)/2;
+}
+
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log( adder(3, 5) )
+
+
+
+// 다른 풀이 2
+function adder(a, b, s = 0){
+  for (var i = Math.min(a, b); i <= Math.max(a, b); i++) s += i;
+  return s;
+}
+
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log( adder(3, 5) )
+
 
 /*
 
