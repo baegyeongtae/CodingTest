27001c7bd01b5850869a0a7a25a6e80a72bae4c0
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-05-04 13:03:10 +0900

    Update 220428-01.js

--- 220428-01.js
+++ 220428-01.js
@@ -9,34 +9,47 @@
 레벨 1
 
 [문제]
-코딩테스트 연습 > 수박수박수박수박수박수?
+코딩테스트 연습 > 연습문제 > 부족한 금액 계산하기
 
 [링크]
-https://programmers.co.kr/learn/courses/30/lessons/12922
+https://programmers.co.kr/learn/courses/30/lessons/82612
 */
 
 
 // 기본 틀
-function solution(n) {
-    var answer = 0;
+function solution(price, money, count) {
+    var answer = -1;
+
     return answer;
 }
 
 
 
 // 풀이
-function solution(s) {
-    return Number(s);
+function solution(price, money, count) {
+    var answer = -1;
+    let sum = 0;
+    for(i=1; i<=count; i++) {
+        sum += price*i
+    }
+    if (money-sum > 0) {return 0}
+    else {return sum-money}
 }
 
 
 
 
-// 다른 풀이 
-const waterMelon = n => "수박".repeat(n).slice(0,n);
+// 다른 풀이 1
+function solution(price, money, count) {
+    const tmp = price * count * (count + 1) / 2 - money;
+    return tmp > 0 ? tmp : 0;
+}
 
-console.log("n이 3인 경우: "+ waterMelon(3))
-console.log("n이 4인 경우: "+ waterMelon(4))
+
+
+// 다른 풀이 2
+const solution = (..._) => Math.max(_[0]*_[2]*++_[2]/2-_[1], 0);
+
 
 /*
 
