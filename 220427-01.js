2d12535f0f7e7b67465d2aaf09a1f57d7a5bb9ee
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-05-04 12:58:42 +0900

    Update 220427-01.js

--- 220427-01.js
+++ 220427-01.js
@@ -9,34 +9,53 @@
 레벨 1
 
 [문제]
-코딩테스트 연습 > 수박수박수박수박수박수?
+코딩테스트 연습 > 연습문제 > 같은 숫자는 싫어
 
 [링크]
-https://programmers.co.kr/learn/courses/30/lessons/12922
+https://programmers.co.kr/learn/courses/30/lessons/12906
 */
 
 
 // 기본 틀
-function solution(n) {
-    var answer = 0;
+function solution(arr)
+{
+    var answer = [];
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+    
     return answer;
 }
 
 
 
 // 풀이
-function solution(s) {
-    return Number(s);
+function solution(arr)
+{
+    var answer = [];
+    answer.push(arr[0])
+    for(i=0; i<arr.length; i++) {
+        if(arr[i] !== arr[i+1]) {
+            if(i+1 === arr.length) {return answer}
+            else {answer.push(arr[i+1])}
+        }
+    }
 }
 
 
 
 
-// 다른 풀이 
-const waterMelon = n => "수박".repeat(n).slice(0,n);
+// 다른 풀이 1
+function solution(arr)
+{
+    return arr.filter((val,index) => val != arr[index+1]);
+}
 
-console.log("n이 3인 경우: "+ waterMelon(3))
-console.log("n이 4인 경우: "+ waterMelon(4))
+
+
+// 다른 풀이 2
+let solution=_=>_.filter((i,$)=>i!=_[--$])
+
 
 /*
 
