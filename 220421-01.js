8e3115087610b658630fd9bb663b9c03a86ebf7d
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-05-04 12:43:11 +0900

    Update 220421-01.js

--- 220421-01.js
+++ 220421-01.js
@@ -1,6 +1,4 @@
 
-
-
 /*
 [플랫폼]
 프로그래머스
@@ -9,15 +7,15 @@
 레벨 1
 
 [문제]
-코딩테스트 연습 > 수박수박수박수박수박수?
+코딩테스트 연습 > 연습문제 > 시저 암호
 
 [링크]
-https://programmers.co.kr/learn/courses/30/lessons/12922
+https://programmers.co.kr/learn/courses/30/lessons/12926
 */
 
 
 // 기본 틀
-function solution(n) {
+function solution(s, n) {
     var answer = 0;
     return answer;
 }
@@ -25,18 +23,55 @@ function solution(n) {
 
 
 // 풀이
-function solution(s) {
-    return Number(s);
+function solution(s, n) {
+    var answer = '';
+    let next = '';
+    for(i=0; i<s.length; i++) {
+        if(s[i] === ' ') {
+            answer = ' '
+            next += answer;
+        } else {
+            answer = s[i].codePointAt(0);
+            if (answer <= 90 && answer >= 65) {
+                answer += n;
+                if (!(answer <= 90 && answer >= 65)) {
+                    answer -= 26;
+                }
+                next += String.fromCharCode(answer);
+            } else if (answer <= 122  && answer >= 97) {
+                answer += n;
+                if (!(answer <= 122 && answer >= 97)) {
+                    answer -= 26;
+                }
+                next += String.fromCharCode(answer);
+            }
+        }
+    }
+    return next;
 }
 
 
 
 
 // 다른 풀이 
-const waterMelon = n => "수박".repeat(n).slice(0,n);
+function solution(s, n) {
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lower = "abcdefghijklmnopqrstuvwxyz";
+    var answer= '';
 
-console.log("n이 3인 경우: "+ waterMelon(3))
-console.log("n이 4인 경우: "+ waterMelon(4))
+    for(var i =0; i <s.length; i++){
+        var text = s[i];
+        if(text == ' ') {
+            answer += ' '; 
+            continue;
+        }
+        var textArr = upper.includes(text) ? upper : lower;
+        var index = textArr.indexOf(text)+n;
+        if(index >= textArr.length) index -= textArr.length;
+        answer += textArr[index];
+    }
+    return answer;
+}
 
 /*
 
