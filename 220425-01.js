9d129d2b944d6a8df778a606921274f94b3bfc03
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-05-04 12:50:28 +0900

    Update 220425-01.js

--- 220425-01.js
+++ 220425-01.js
@@ -9,34 +9,62 @@
 레벨 1
 
 [문제]
-코딩테스트 연습 > 수박수박수박수박수박수?
+코딩테스트 연습 > 연습문제 > 문자열 내림차순으로 배치하기
 
 [링크]
-https://programmers.co.kr/learn/courses/30/lessons/12922
+https://programmers.co.kr/learn/courses/30/lessons/12917
 */
 
 
 // 기본 틀
-function solution(n) {
-    var answer = 0;
+function solution(s) {
+    var answer = '';
     return answer;
 }
 
 
 
 // 풀이
-function solution(s) {
-    return Number(s);
+function solution(s){
+    var answer = true;
+    let pcount = 0;
+    let ycount = 0;
+    for(i=0; i<s.length; i++) {
+        if(s[i] === 'p' || s[i] === 'P') {
+            pcount++;
+        } else if (s[i] === 'y' || s[i] === 'Y') {
+            ycount++;
+        }
+    }
+    if (pcount === ycount) {return answer;}
+    else {return !answer;}
 }
 
 
 
 
-// 다른 풀이 
-const waterMelon = n => "수박".repeat(n).slice(0,n);
+// 다른 풀이 1
+function numPY(s){
+  //함수를 완성하세요
+    return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
+}
 
-console.log("n이 3인 경우: "+ waterMelon(3))
-console.log("n이 4인 경우: "+ waterMelon(4))
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log( numPY("pPoooyY") )
+console.log( numPY("Pyy") )
+
+
+
+// 다른 풀이 2
+function numPY(s) {
+  return s.match(/p/ig).length == s.match(/y/ig).length;
+}
+
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log( numPY("pPoooyY") )
+console.log( numPY("Pyy") )
 
 /*
 
