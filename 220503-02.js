253eb754826e0b501fa2c6992aa497da3c389a61
Bae Gyeongtae <39544482+baegyeongtae@users.noreply.github.com>
2022-05-13 22:23:51 +0900

    Update 220503-02.js

--- 220503-02.js
+++ 220503-02.js
@@ -1,1 +1,8 @@
 
+function solution(d, budget) {
+    let answer = 0;
+    let arr = d.sort((a, b) => a-b);
+    let sum = 0;
+    while (arr.reduce((acc, cur) => (acc+cur),0) > budget) arr.pop();
+    return arr.length
+}
